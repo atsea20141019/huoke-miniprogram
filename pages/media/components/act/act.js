b85f3b13c9fc08f3f32b69da82b8ca4081748735
xiangjianfeng <xiangjianfeng@xiangjianfengdeMacBook-Pro.local>
2021-05-01 23:35:21 +0800

    修复动态登录问题

--- pages/media/components/act/act.js
+++ pages/media/components/act/act.js
@@ -16,7 +16,8 @@ Component({
    */
   data: {
     goodsDetail: {},
-    coverImgArr: ''
+    coverImgArr: '',
+    notlogin: true
   },
 
   // observers: {
@@ -47,10 +48,88 @@ Component({
         })
       })
     },
+
+    getPhone(e) {
+      let _this = this
+      wx.getStorage({
+        key: 'token',
+        success: res => {
+          _this.getPhoneNumber(e)
+        },
+        fail: () => {
+          console.log('没有登录')
+          wx.getUserProfile({
+            desc: '业务需要',
+            success: res1 => {
+              wx.login({
+                success: codeRes => {
+                  console.log(codeRes)
+                  if (codeRes.code) {
+                    axios.post('/wxc/wx/mini_login', {
+                      code: codeRes.code,
+                      iv: e.detail.iv,
+                      data: e.detail.encryptedData
+                    }).then(res => {
+                      if (res.code === 200) {
+                        wx.setStorageSync('token', res.data.token)
+                        wx.setStorageSync('client_id', res.data.client_id)
+                        wx.setStorageSync('wechat_avatar', res.data.wechat_avatar)
+                        wx.setStorageSync('wechat_nickname', res.data.wechat_nickname)
+                        app.globalData.token = res.data.token
+                        app.globalData.client_id = res.data.client_id
+                        _this.getPhoneNumber(e)
+                      }
+                    })
+                  }
+                }
+              })
+            }
+          })
+        }
+      })
+    },
+
+    getUserProfile(e) {
+      let _this = this
+      wx.getUserProfile({
+        desc: '业务需要',
+        success: res1 => {
+          wx.login({
+            success: codeRes => {
+              if (codeRes.code) {
+                axios.post('/wxc/wx/mini_login', {
+                  code: codeRes.code,
+                  iv: res1.iv,
+                  data: res1.encryptedData
+                }).then(res => {
+                  if (res.code === 200) {
+                    wx.setStorageSync('token', res.data.token)
+                    wx.setStorageSync('client_id', res.data.client_id)
+                    wx.setStorageSync('wechat_avatar', res.data.wechat_avatar)
+                    wx.setStorageSync('wechat_nickname', res.data.wechat_nickname)
+                    wx.setStorageSync('card_status', res.data.card_status)
+                    app.globalData.token = res.data.token
+                    app.globalData.client_id = res.data.client_id
+
+                    _this.setData({
+                      notlogin: false
+                    })
+
+                  }
+                })
+              }
+            }
+          })
+        }
+      })
+
+    },
+
     getPhoneNumber(e) {
       let _this = this
       wx.login({
         success: res => {
+
           axios.post('/wxc/wx/mini_phone', {
             code: res.code,
             iv: e.detail.iv,
@@ -90,11 +169,11 @@ Component({
                 _this.triggerEvent('myevent', 1)
               }
             })
-          }else{
+          } else {
             this.triggerEvent('myevent', res.data.status)
           }
 
-          
+
         } else {
           wx.showToast({
             title: res.msg,
